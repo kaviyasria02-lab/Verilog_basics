2719a93fc9f5376b3dd7bb99d79ce11c30c28599
Kaviyasri A <kaviyasria02@gmail.com>
2026-02-28 18:54:14 +0530

    Add testbench for AND gate module

--- AND.v
+++ AND.v
@@ -3,3 +3,20 @@ module andlogic (A,B,C);
   output C;
   assign C=A&B;
 endmodule
+
+#Testbench
+
+module andlogic_tb;
+  reg A,B;
+  wire C;
+  andlogic dut(A,B,C);
+  initial begin
+       A=0;B=0;
+    #5 A=0;B=1;
+    #5 A=1;B=0;
+    #5 A=1;B=1;
+    #100 $finish;
+  end
+  always@(A,B)
+    $display("time=%0t,a=%b,b=%b,c=%b",$time,A,B,C);
+endmodule
