577ac61ce794ba70797a15520e48d02e141698e9
Kaviyasri A <kaviyasria02@gmail.com>
2026-02-28 19:12:07 +0530

    Add XNOR logic module and testbench

---
+++ XNOR.v
@@ -0,0 +1,21 @@
+module xnorlogic(A,B,C);
+  input A,B;
+  output C;
+  assign C=~(A^B);
+endmodule
+
+#Testbench
+
+module xnorlogic_tb;
+  reg A,B;
+  wire C;
+  norlogic dut(A,B,C);
+  initial begin
+    A=0;B=0;
+    #5 A=0;B=1;
+    #5 A=1;B=0;
+    #5 A=1;B=1;
+  end
+  always@(A,B,C)
+    $display("a=%b,b=%b,C=%b",A,B,C);
+endmodule
