42baac1875de33d6149c32932c5241be572be4e3
Kaviyasri A <kaviyasria02@gmail.com>
2026-02-28 18:56:35 +0530

    Create NOT.v

---
+++ NOT.v
@@ -0,0 +1,20 @@
+module notlogic(A,B);
+  input A;
+  output B;
+  assign B=~A;
+endmodule
+
+#testbench
+
+module notlogic_tb;
+  reg A;
+  wire B;
+  notlogic dut (A,B);
+  initial begin
+       A=0;
+    #5 A=1;
+    #50 $finish;
+  end
+  always@(A)
+    $display("time=%0t,A=%b,B=%b",$time,A,B);
+endmodule
